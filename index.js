53dcc0dd881898e59b934deee13c2205e46d1197
Takuya Matsuyama <nora@odoruinu.net>
2016-05-07 23:58:38 +0900

    export file and dlopen it

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 /*
 	MIT License http://www.opensource.org/licenses/mit-license.php
 	Author Tobias Koppers @sokra
+	Author Takuya Matsuyama @noradaiko
 */
-module.exports = function() {
-	return "try {process.dlopen(" + JSON.stringify(this.resourcePath) + ", module.exports); } catch(e) {" +
-		"throw new Error('Cannot open ' + " + JSON.stringify(this.resourcePath) + " + ': ' + e);}";
+var loaderUtils = require("loader-utils");
+
+module.exports = function(content) {
+	this.cacheable && this.cacheable();
+	if(!this.emitFile) throw new Error("emitFile is required from module system");
+	var query = loaderUtils.parseQuery(this.query);
+	var url = loaderUtils.interpolateName(this, query.name || "[hash].[ext]", {
+		context: query.context || this.options.context,
+		content: content,
+		regExp: query.regExp
+	});
+	this.emitFile(url, content);
+	return "try {process.dlopen(module, __dirname + '/' + " + JSON.stringify(url) + "); } catch(e) {" +
+		"throw new Error('Cannot open ' + " + JSON.stringify(url) + " + ': ' + e);}";
 }
+
+module.exports.raw = true;
